b0259ef4539a8b75e7cde431414249e48cf85bfc
Jonathan Fernandez <jfern20@gmail.com>
2020-12-06 06:32:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,16 +1,17 @@
 
 def speak_to_grandma(phrase)
+  
 if phrase== "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 
 elsif phrase== "Hi Nana, how are you?"
  return "HUH?! SPEAK UP, SONNY!"
  
- elsif phrase== "Hi!"
+elsif phrase== "Hi!"
   return "HUH?! SPEAK UP, SONNY!"
   
  
- elsif phrase=="WHAT DID YOU EAT TODAY?"
+elsif phrase=="WHAT DID YOU EAT TODAY?"
    return "NO, NOT SINCE 1938!"
   
  
@@ -20,5 +21,3 @@ elsif phrase== "Hi Nana, how are you?"
   
  end
 end 
-
-
